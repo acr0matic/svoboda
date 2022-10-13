ebaa50d7954090b2d8d5743c5fffa7b2e6ae5f80
acr0matic <work.acr0matic@gmail.com>
2022-10-13 12:49:43 +0300

    feat: обновление ядра проекта

--- gulp_tasks/minify_css.js
+++ gulp_tasks/minify_css.js
@@ -2,16 +2,23 @@ const { task, src, dest } = require('gulp');
 const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
 const purgecss = require('gulp-purgecss');
+const gcmq = require('gulp-group-css-media-queries');
 
 const paths = require('../gulpfile');
 
-// Задача для минификации кода стилей для сокращения скорости загрузки веб-страницы
+/*
+- Удаление неиспользуемых стилей
+- Минификация кода
+- Переименовывание выходного файла
+*/
+
 task('minify_css', () => src(paths.styles.temp + paths.styles.out)
   .pipe(
     purgecss({
       content: ['src/**/*.{html,php}', 'src/**/*.js'],
     }),
   )
+  .pipe(gcmq())
   .pipe(cleanCSS({ level: 2 }))
   .pipe(rename(paths.styles.minify))
   .pipe(dest(paths.styles.dist)));
