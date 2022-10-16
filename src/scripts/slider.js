66ea31df28fc760c3e08cb012890ae6522987d02
acr0matic <work.acr0matic@gmail.com>
2022-10-16 19:41:44 +0300

    feat: верстка "команда"

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,12 +1,29 @@
 function InitSlider() {
+  const team = new Swiper('.slider-team', {
+    slidesPerView: 1,
+    loop: true,
+    simulateTouch: false,
+
+    navigation: {
+      nextEl: '.team__slider .swiper-button-next',
+      prevEl: '.team__slider .swiper-button-prev',
+    },
+
+    on: {
+      slideChangeTransitionStart() {
+        lazyLoadInstance.update();
+      }
+    }
+  });
+
   const interior = new Swiper('.slider-interior', {
     slidesPerView: 'auto',
     spaceBetween: 80,
     simulateTouch: false,
 
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.interior__slider .swiper-button-next',
+      prevEl: '.interior__slider .swiper-button-prev',
     },
   });
 }
