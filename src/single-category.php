0ab6b6a539c79c59568682fb18b4270ea0ae66d6
acr0matic <work.acr0matic@gmail.com>
2022-11-03 14:57:46 +0300

    feat: переработка "о нас"

---
+++ src/single-category.php
@@ -0,0 +1,148 @@
+<?php include_once('layout/partials/header.php') ?>
+
+<main id="single-service">
+  <section id="service-list" class="section">
+    <div class="container">
+      <div class="section__header">
+        <h2 class="section__title">
+          Эстетическая и аппаратная косметология
+        </h2>
+      </div>
+      <!-- /.section__header -->
+
+      <div class="service-list">
+        <div class="service-list__accordion handorgel">
+          <div class="handorgel__header">
+            <button class="handorgel__header__button">
+              <h3 class="handorgel__header__text">
+                Маникюр без покрытия
+              </h3>
+              <!-- /.handorgel__header__text -->
+
+              <div class="handorgel__indicator">
+                <img alt="" class="handorgel__arrow" src="img/icons/control/dropdown__cross.svg">
+              </div>
+              <!-- /.handorgel__indicator -->
+            </button>
+          </div>
+          <!-- /.handorgel__header -->
+
+          <div class="handorgel__content" data-open>
+            <div class="handorgel__content__inner">
+              <div class="row">
+                <div class="col-12 col-md-8">
+                  <div data-editor="default" class="service-list__content">
+                    <p>
+                      Описание услуги уникальное пространство красоты, в котором слились в одно целое высококачественный сервис и искусство создания истинно элегантных и стильных образов с учетом последних тенденций и индивидуальных особенностей каждого клиента.
+                    </p>
+                  </div>
+                </div>
+                <!-- /.col-12 col-md-8 -->
+              </div>
+              <!-- /.row -->
+
+              <div class="col-12">
+                <div class="service-list__footer">
+                  <div class="service-list__price"><span>2000</span> ₽</div>
+                  <button data-micromodal-trigger="modal-callback" class="button button-outline">Записаться</button>
+                </div>
+                <!-- /.service-list__footer -->
+              </div>
+              <!-- /.col-12 -->
+            </div>
+          </div>
+        </div>
+
+        <div class="service-list__accordion handorgel">
+          <div class="handorgel__header">
+            <button class="handorgel__header__button">
+              <h3 class="handorgel__header__text">
+                Маникюр покрытие гель лак
+              </h3>
+              <!-- /.handorgel__header__text -->
+
+              <div class="handorgel__indicator">
+                <img alt="" class="handorgel__arrow" src="img/icons/control/dropdown__cross.svg">
+              </div>
+              <!-- /.handorgel__indicator -->
+            </button>
+          </div>
+          <!-- /.handorgel__header -->
+
+          <div class="handorgel__content">
+            <div class="handorgel__content__inner">
+              <div class="row">
+                <div class="col-12 col-md-8">
+                  <div data-editor="default" class="service-list__content">
+                    <p>
+                      Описание услуги уникальное пространство красоты, в котором слились в одно целое высококачественный сервис и искусство создания истинно элегантных и стильных образов с учетом последних тенденций и индивидуальных особенностей каждого клиента.
+                    </p>
+                  </div>
+                </div>
+                <!-- /.col-12 col-md-8 -->
+              </div>
+              <!-- /.row -->
+
+              <div class="col-12">
+                <div class="service-list__footer">
+                  <div class="service-list__price"><span>2000</span> ₽</div>
+                  <button data-micromodal-trigger="modal-callback" class="button button-outline">Записаться</button>
+                </div>
+                <!-- /.service-list__footer -->
+              </div>
+              <!-- /.col-12 -->
+            </div>
+          </div>
+        </div>
+
+        <div class="service-list__accordion handorgel">
+          <div class="handorgel__header">
+            <button class="handorgel__header__button">
+              <h3 class="handorgel__header__text">
+                Маникюр без покрытия
+              </h3>
+              <!-- /.handorgel__header__text -->
+
+              <div class="handorgel__indicator">
+                <img alt="" class="handorgel__arrow" src="img/icons/control/dropdown__cross.svg">
+              </div>
+              <!-- /.handorgel__indicator -->
+            </button>
+          </div>
+          <!-- /.handorgel__header -->
+
+          <div class="handorgel__content">
+            <div class="handorgel__content__inner">
+              <div class="row">
+                <div class="col-12 col-md-8">
+                  <div data-editor="default" class="service-list__content">
+                    <p>
+                      Описание услуги уникальное пространство красоты, в котором слились в одно целое высококачественный сервис и искусство создания истинно элегантных и стильных образов с учетом последних тенденций и индивидуальных особенностей каждого клиента.
+                    </p>
+                  </div>
+                </div>
+                <!-- /.col-12 col-md-8 -->
+              </div>
+              <!-- /.row -->
+
+              <div class="col-12">
+                <div class="service-list__footer">
+                  <div class="service-list__price"><span>2000</span> ₽</div>
+                  <button data-micromodal-trigger="modal-callback" class="button button-outline">Записаться</button>
+                </div>
+                <!-- /.service-list__footer -->
+              </div>
+              <!-- /.col-12 -->
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /.service-list -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <?php include_once('layout/blocks/contact.php') ?>
+</main>
+
+<?php include_once('layout/partials/footer.php') ?>
